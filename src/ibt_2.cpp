0b8bcd7c617c4f238356db03ca255db0c5c50fdb
Phakin Kongkha <ntrain5555@gmail.com>
2025-04-25 23:30:05 +0700

    Change PWM handling

--- src/ibt_2.cpp
+++ src/ibt_2.cpp
@@ -13,25 +13,27 @@ IBT_2::IBT_2(uint8_t pin_pwm_l, uint8_t pin_pwm_r) : pin{pin_pwm_l, pin_pwm_r},
 			uint8_t pwm_curr = ibt_2->pwm.curr;
 			while (true) {
 				if ((pwm_curr == ibt_2->pwm.curr) && (direction == ibt_2->pwm.direction)) {
+					vTaskDelay(5 / portTICK_PERIOD_MS);
 					continue;
 				}
 				pwm_curr = ibt_2->pwm.curr;
 				direction = ibt_2->pwm.direction;
 #ifdef DEBUG
-				Serial.printf("PWM: %hd, DIR: %s\n",
-					ibt_2->pwm.curr,
-					ibt_2->pwm.direction == FORWARD ? "FORWARD" : "BACKWARD"
+				Serial.printf("DIR: %s, PWM: %hd\n",
+					ibt_2->pwm.direction == FORWARD ? "FORWARD" : "BACKWARD",
+					ibt_2->pwm.curr
 				);
 #endif
 				switch (ibt_2->pwm.direction) {
 					case FORWARD:
-						analogWrite(ibt_2->pin.pwm_l, 255 - ibt_2->pwm.curr); // The transistor is pull up
-						analogWrite(ibt_2->pin.pwm_r, 255); // The transistor is pulling down
+						analogWrite(ibt_2->pin.pwm_l, ibt_2->pwm.curr);
+						analogWrite(ibt_2->pin.pwm_r, 0);
 						break;
 					case BACKWARD:
-						analogWrite(ibt_2->pin.pwm_l, 255); // The transistor is pulling down
-						analogWrite(ibt_2->pin.pwm_r, 255 - ibt_2->pwm.curr); // The transistor is pull up
+						analogWrite(ibt_2->pin.pwm_l, 0);
+						analogWrite(ibt_2->pin.pwm_r, ibt_2->pwm.curr);
 						break;
 				}
-			} }, "pwm Retaining", 4000, this, 1, NULL, 1);
+				vTaskDelay(5 / portTICK_PERIOD_MS);
+			} }, "PWM Retaining", 2000, this, 1, NULL, 1);
 }
